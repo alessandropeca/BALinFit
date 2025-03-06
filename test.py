336bff5b41a496d6e8f6997031993954a96c3c90
Alessandro Peca <peca.alessandro@gmail.com>
2025-03-06 16:26:18 -0500

    Update test.py

--- test.py
+++ test.py
@@ -2,22 +2,16 @@ import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt
 
-
-
 from BAsymLinFit import *
-
-
 
 
 df = pd.read_csv('test_emcee.csv', names=['names', 'x', 'xerrl', 'xerru', 'y', 'yerrl', 'yerru'], header=0)
 df = df = df[~df['x'].isna() & (df['x'] != -np.inf)]
 
-
-
 results = bayesian_regression_mcmc(
     x=df['x'].values, 
     y=df['y'].values, 
-    y_err_lower=df['yerrl'].values/1.645, 
+    y_err_lower=df['yerrl'].values/1.645, # moving to 1 sigma errors, needed for Gaussian shape
     y_err_upper=df['yerru'].values/1.645,
     x_err_lower=df['xerrl'].values/1.645, 
     x_err_upper=df['xerru'].values/1.645,
